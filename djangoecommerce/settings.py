4ea2fa15076fd05f4841ed3253002d7c2cf60e34
Dan-source <dan.dluis.dl@gmail.com>
2021-01-21 09:21:31 -0300

    vai troco

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -128,5 +128,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from .core.views import index, contact, product, product_list
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('/', index, name='index'),
     path('contato/', contact, name='contact'),
     path('produto/',product, name='product'),
     path('produtos/', product_list, name='product_list'),
