b5182a1c5d5ae9458aebeae5c704954e4a973ddf
Dan-source <dan.dluis.dl@gmail.com>
2021-01-27 17:56:31 -0300

    add login patterns

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
 
-# Create your views here.
+
+class RegisterView(CreateView):
+
+    model = User
+    template_name = 'accounts/register.html'
+    form_class = UserAdminCreationForm
+    success_url = reverse_lazy('index')
