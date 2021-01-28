131a85cfa887424566bf2b5a526738b9a99385c2
Dan-source <dan.dluis.dl@gmail.com>
2021-01-28 09:30:58 -0300

    add alterar dados

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, re_path
 
-from . import views 
+from . import views
 
 app_name = 'accounts'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('alterar-dados', views.update_user, name='update_user'),
     path('registro', views.register, name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse_lazy
 from django.shortcuts import render
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import User
 from .forms import UserAdminCreationForm
@@ -16,5 +16,17 @@ class RegisterView(CreateView):
     form_class = UserAdminCreationForm
     success_url = reverse_lazy('index')
 
+class UpdateUserView(UpdateView):
+
+    model = User
+    template_name = 'accounts/update_user.html'
+    fields = ['name', 'email']
+    success_url = reverse_lazy('accounts:index')
+
+    def get_object(self):
+        return self.request.user
+
+
 register = RegisterView.as_view()
+update_user = UpdateUserView.as_view()
 index = IndexView.as_view()
