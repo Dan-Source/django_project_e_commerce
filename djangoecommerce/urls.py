97a9822310f88bd1aac9086813bec458fc95eded
Dan-source <dan.dluis.dl@gmail.com>
2021-03-30 11:22:38 -0300

    add decouple

--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -18,6 +18,6 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(
-        settings.MEDIA_URL, server,
+        settings.MEDIA_URL, serve,
         document_root=settings.MEDIA_ROOT
     )
