554010366635c8393a8a8bd55a095f7810765b9b
Dan-source <dan.dluis.dl@gmail.com>
2021-04-03 09:42:22 -0300

    add urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('alterar-dados/', views.update_user, name='update_user'),
     path('alterar-senha/', views.update_password, name='update_password'),
-    path('registro', views.register, name='register'),
+    path('registro/', views.register, name='register'),
     path('entrar/', views.login, name='login'),
     path('sair/', views.logout, name='logout'),
     ]
